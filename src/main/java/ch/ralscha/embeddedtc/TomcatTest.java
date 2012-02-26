e62da3abeab0efcc8c6cfa74819520732834d393
Ralph Schaer <ralphschaer@gmail.com>
2012-02-26 09:13:49 +0100

    add license header and set privileged flag to true

--- src/main/java/ch/ralscha/embeddedtc/TomcatTest.java
+++ src/main/java/ch/ralscha/embeddedtc/TomcatTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2012 Ralph Schaer <ralphschaer@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ch.ralscha.embeddedtc;
 
 import org.junit.AfterClass;
@@ -20,6 +35,7 @@ public class TomcatTest {
 	public static void startServer() {
 		et = new EmbeddedTomcat(9998);
 		et.setSilent(true);
+		et.setPrivileged(true);
 		et.dontAddShutdownHook();
 		et.start();
 	}
