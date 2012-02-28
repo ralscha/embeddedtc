786fe0a235e78e624d6324231f590f22f67ab914
Ralph Schaer <ralphschaer@gmail.com>
2012-02-28 06:11:33 +0100

    added license header

--- src/main/java/ch/ralscha/embeddedtc/ZipDirContext.java
+++ src/main/java/ch/ralscha/embeddedtc/ZipDirContext.java
@@ -1,21 +1,19 @@
-package ch.ralscha.embeddedtc;
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+/**
+ * Copyright 2012 Ralph Schaer <ralphschaer@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package ch.ralscha.embeddedtc;
 
 import java.io.File;
 import java.io.IOException;
@@ -56,7 +54,7 @@ import org.apache.naming.resources.ResourceAttributes;
  * @author Remy Maucherat
  * @version $Id: ZipDirContext.java 1081987 2011-03-15 23:05:53Z markt $
  */
-@SuppressWarnings(value={"javadoc","hiding"})
+@SuppressWarnings(value = { "javadoc", "hiding" })
 class ZipDirContext extends BaseDirContext {
 
 	private static final org.apache.juli.logging.Log log = org.apache.juli.logging.LogFactory
