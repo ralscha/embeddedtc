41bb444c0a72e5eeed35b77679de16a80058ebdd
Ralph Schaer <ralphschaer@gmail.com>
2012-07-09 08:35:52 +0200

    Moved to a new package ch.rasc

--- src/main/java/ch/rasc/embeddedtc/Artifact.java
+++ src/main/java/ch/rasc/embeddedtc/Artifact.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ch.ralscha.embeddedtc;
+package ch.rasc.embeddedtc;
 
 class Artifact {
 	private final String groupId;
